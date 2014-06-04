d22b509e042627195db61e9393cd285b1651a53a
Richard Lee <rblee88@gmail.com>
2014-06-03 23:15:45 -0500

    sending events directly to SS
    SS still doesn't seem to pick up any of the events.  The PSN code seems to
    work though since I was able to get it to interact with Steam

--- surgeon.py
+++ surgeon.py
@@ -30,10 +30,59 @@ def open_hand():
     k.release_key('space')
 
 
+from AppKit import NSWorkspace
+from Quartz.CoreGraphics import CGEventCreateMouseEvent
+from Quartz.CoreGraphics import CGEventPost
+from Quartz.CoreGraphics import CGEventPostToPSN
+from Quartz.CoreGraphics import CGDisplayShowCursor
+from Quartz.CoreGraphics import kCGEventMouseMoved
+from Quartz.CoreGraphics import kCGEventLeftMouseDown
+from Quartz.CoreGraphics import kCGEventLeftMouseUp
+from Quartz.CoreGraphics import kCGMouseButtonLeft
+from Quartz.CoreGraphics import kCGHIDEventTap
+from Quartz.CoreGraphics import kCGEventTargetProcessSerialNumber
+from Quartz.CoreGraphics import CGEventTapCreateForPSN
+from Quartz.CoreGraphics import CGEventGetIntegerValueField
+
+from ctypes import c_int
+from ctypes import Structure
+
+
+class ProcessSerialNumber(Structure):
+    _fields_ = [("highLongOfPSN", c_int),
+                ("lowLongOfPSN", c_int)]
+
+
+def mouseEvent(type, posx, posy):
+    surgeon_process = filter(lambda x: x, [x if x.get('NSApplicationName') == 'Surgeon Simulator 2013' else None for x in  NSWorkspace.sharedWorkspace().launchedApplications()])[0]
+    #surgeon_process = filter(lambda x: x, [x if x.get('NSApplicationName') == 'Steam' else None for x in  NSWorkspace.sharedWorkspace().launchedApplications()])[0]
+    high_psn = surgeon_process.get('NSApplicationProcessSerialNumberHigh')
+    low_psn = surgeon_process.get('NSApplicationProcessSerialNumberLow')
+    surgeon_psn = (high_psn, low_psn)
+
+    #surgeon_psn = (low_psn, high_psn)
+    #print(surgeon_process)
+    #print(surgeon_psn)
+    theEvent = CGEventCreateMouseEvent(None, type, (posx, posy), kCGMouseButtonLeft)
+    #CGEventPost(kCGHIDEventTap, theEvent)
+    CGEventPostToPSN(surgeon_psn, theEvent)
+
+
+def mousemove(posx, posy):
+    mouseEvent(kCGEventMouseMoved, posx, posy)
+
+def mouseclick(posx,posy):
+    #mouseEvent(kCGEventMouseMoved, posx,posy); #uncomment this line if you want to force the mouse to MOVE to the click location first (i found it was not necesary).
+    mouseEvent(kCGEventLeftMouseDown, posx,posy);
+    mouseEvent(kCGEventLeftMouseUp, posx,posy);
+
+
 def moveMouse(x, y):
     bndl = objc.loadBundle('CoreGraphics', globals(), '/System/Library/Frameworks/ApplicationServices.framework')
     objc.loadBundleFunctions(bndl, globals(), [('CGWarpMouseCursorPosition', 'v{CGPoint=ff}')])
     CGWarpMouseCursorPosition((x, y))
+
+#filter(lambda x: x, [x if x.get('NSApplicationName') == 'Surgeon Simulator 2013' else None for x in  NSWorkspace.sharedWorkspace().launchedApplications()])
 
 
 class SurgeonListener(Leap.Listener):
@@ -41,22 +90,27 @@ class SurgeonListener(Leap.Listener):
         super(SurgeonListener, self).__init__()
 
     def on_frame(self, controller):
+        CGDisplayShowCursor(1)
         frame = controller.frame()
         if not frame.hands.is_empty:
             hand = frame.hands[0]
             position = hand.palm_position
             cur_x, cur_y = m.position()
             if position.y < 200:
-                m.click(x_dim/2, y_dim/2, 1)
+                pass
+                #m.click(x_dim/2, y_dim/2, 1)
+                mouseclick(x_dim/2, y_dim/2)
 
             if position.x < 0:
                 print(cur_x)
+                mousemove(cur_x-1, cur_y)
                 #moveMouse(0, int(cur_y))
                 #m.move(cur_x-1, cur_y)
-                autopy.mouse.smooth_move(0, 0)
+                #autopy.mouse.smooth_move(0, 0)
 
             if position.x > 0:
-                autopy.mouse.smooth_move(int(x_dim2)-1, int(y_dim2)-1)
+                mousemove(cur_x+1, cur_y)
+                #autopy.mouse.smooth_move(int(x_dim2)-1, int(y_dim2)-1)
                 print(cur_x)
                 #moveMouse(x_dim, int(cur_y))
                 #m.move(cur_x+1, cur_y)
